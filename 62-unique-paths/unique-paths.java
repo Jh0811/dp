60c98cb3a585526c63295dc710d8206e7cdfbddb
Jhalsa Mishra <69795201+Jh0811@users.noreply.github.com>
2024-06-05 05:56:57 +0530

    Time: 1 ms (11.43%) | Memory: 40.2 MB (65.57%) - LeetSync

--- 62-unique-paths/unique-paths.java
+++ 62-unique-paths/unique-paths.java
@@ -1,25 +1,33 @@
 class Solution {
-    static int UniquePathsuntil(int i, int j, int [][]dp)
+    public int countWaysutil(int m, int n,int [][]dp)
     {
-        if(i==0 && j==0) return 1;
-         
-         if(i<0 || j<0) return 0;
+        for(int i=0;i<m;i++)
+        {
+            for(int j=0;j<n;j++)
+            {
+                if(i==0 && j==0){
+                    dp[i][j]=1;
+                    continue;
+                }
+                int up =0;
+                int left =0;
+                if(i>0)
+                 up = dp[i-1][j];
+                 if(j>0)
+                 left = dp[i][j-1];
+                 dp[i][j] = up+left;
+            }
 
-         if(dp[i][j]!=-1)
-         return dp[i][j];
-
-         int up = UniquePathsuntil(i-1,j,dp);
-         int left = UniquePathsuntil(i,j-1,dp);
-
-         return dp[i][j] = up+left;
-
+        }
+        return dp[m-1][n-1];
     }
     public int uniquePaths(int m, int n) {
-        int dp[][] = new int[m][n];
 
-        for(int row[]:dp)
-        Arrays.fill(row, -1);
-
-        return UniquePathsuntil(m-1,n-1,dp);
+        int dp [][] = new int[m][n];
+        for(int [] row:dp)
+        {
+            Arrays.fill(row,-1);
+        }
+        return countWaysutil(m,n,dp);
     }
 }

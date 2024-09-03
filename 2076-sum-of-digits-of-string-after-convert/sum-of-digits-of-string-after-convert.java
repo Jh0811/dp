91dd290ba6969dcbe6f021ef59fcf0d30996365b
Jhalsa Mishra <69795201+Jh0811@users.noreply.github.com>
2024-09-04 05:28:26 +0530

    Time: 3 ms (37.54%) | Memory: 43.2 MB (21.84%) - LeetSync

---
+++ 2076-sum-of-digits-of-string-after-convert/sum-of-digits-of-string-after-convert.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int getLucky(String s, int k) {
+          String numericString = "";
+        for (char ch : s.toCharArray()) {
+            numericString += Integer.toString(ch - 'a' + 1);
+        }
+
+        // Apply digit sum transformations k times
+        while (k-- > 0) {
+            int digitSum = 0;
+            for (char digit : numericString.toCharArray()) {
+                digitSum += digit - '0';
+            }
+            numericString = Integer.toString(digitSum);
+        }
+
+        // Convert the final string to integer and return
+        return Integer.parseInt(numericString);
+    }
+}
